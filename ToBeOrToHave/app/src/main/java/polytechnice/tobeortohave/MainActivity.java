6b7cf9dd69769273d2a730f67b9542c6c7dc1cbe
pierrebonny <pierre.bonny@etu.unice.fr>
2017-05-20 10:16:07 +0200

    testCommit

--- ToBeOrToHave/app/src/main/java/polytechnice/tobeortohave/MainActivity.java
+++ ToBeOrToHave/app/src/main/java/polytechnice/tobeortohave/MainActivity.java
@@ -51,7 +51,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         repeatAction = new RepeatAction(this);
-        Noti
     }
 
     @Override
